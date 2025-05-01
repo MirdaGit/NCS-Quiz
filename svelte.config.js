be160b43bd5561b6c5040f70aca61c2913ac84e1
MirdaGit <M.Ste2@seznam.cz>
2025-05-02 00:09:51 +0200

    Fix build dir name

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,13 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
+		appDir: "app",
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? 'https://mirdagit.github.io/NCS-Quiz/' : ''
+			base: process.env.NODE_ENV === "production" ? "/NCS-Quiz" : ""
 		}
 	}
 };
