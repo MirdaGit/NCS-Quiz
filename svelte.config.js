487b9c80601d326401871d718f332299d31c07a6
MirdaGit <M.Ste2@seznam.cz>
2025-05-01 21:59:51 +0200

    Update path

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/NCS-Quiz' : ''
+			base: process.env.NODE_ENV === 'production' ? 'https://mirdagit.github.io/NCS-Quiz/' : ''
 		}
 	}
 };
